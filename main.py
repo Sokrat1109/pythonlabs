e47e9d8793a61a0a0f19377e4221fd53491f86e7
Sokrat1109 <146949643+Sokrat1109@users.noreply.github.com>
2024-03-20 09:45:36 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,20 @@
+﻿def Fact(a):
+    if a <= 1:
+        return 1
+    else:
+        return a*Fact(a-1) # рекурсия
+
+n = int(input())
+s = ['']*(n+1)
+
+for i in range(0, n+1):
+    for j in range(0, i+1):
+        s[i] += str(int(Fact(i)/(Fact(j)*Fact(i-j)))) + ' '
+    s[i] = s[i][:-1:]
+
+counter = len(s[n])
+
+for i in range(0, n + 1):
+    c = (counter-len(s[i]))//2
+    l = ' ' * c + s[i] + ' ' * c
+    print(l)
