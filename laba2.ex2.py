14d11fe25ba2141c8075e637256349859b2cc486
Sokrat1109 <146949643+Sokrat1109@users.noreply.github.com>
2024-03-20 15:39:33 +0200

    Update laba2.ex2.py

--- laba2.ex2.py
+++ laba2.ex2.py
@@ -1,14 +1,28 @@
-s1 = '.*.'
-s2 = '*.*'
+def Fact(a):
+    if a <= 1:
+        return 1
+    else:
+        return a*Fact(a-1) # рекурсия
+
 
 n = int(input())
+n = 2**(1+n)
+s = ['']*(n+1)
 
-count = 2**(n-1)
+for i in range(0, n+1):
+    for j in range(0, i+1):
+        c_k_n = int(Fact(i)/(Fact(j)*Fact(i-j)))
+        if c_k_n % 2 == 1:
+            c_k_n = '*'
+        else:
+            c_k_n = ' '
+        s[i] += c_k_n + ' '
+    s[i] = s[i][:-1:]
 
-s_space = '...'
 
-for i in range (1, count+1):
-    print(s1 * i)
-    print(s2 * i)
-    if%2
+counter = len(s[n])
 
+for i in range(0, n ):
+    c = (counter-len(s[i]))//2
+    l = ' ' * c + s[i] + ' ' * c
+    print(l)
